88056bcb8be2ad867f7d1e13cef0a612b37b651c
Anton_home_PC <mymail>
2023-03-07 00:48:18 +0300

    прием и отправка

--- net_tst/net_tst.cpp
+++ net_tst/net_tst.cpp
@@ -5,9 +5,16 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
 #pragma comment(lib, "Ws2_32.lib")
-
+std::string int_to_string_ip(int ip)
+{
+	return std::to_string((ip >> 24) & 0xff) + "." + \
+		std::to_string((ip >> 16) & 0xff) + "." + \
+		std::to_string((ip >> 8) & 0xff) + "." + \
+		std::to_string((ip >> 0) & 0xff) + ".";
+}
 int main() {
 	//Определяем версию сокетов ОС
 	WSADATA wsData;
@@ -46,5 +53,46 @@ int main() {
 		std::cout << "failed to set non blocking socket\n";
 		return  false;
 	}
-	return 0;
+	bool go = true;
+	std::string cmd = "";
+	while (go)
+	{
+		std::cout << "enter cmd (rx, tx, ex): ";
+		std::cin >> cmd;
+		if (cmd == "tx")
+		{
+			std::string tx_data = "";
+			std::cout << "enter a message: ";
+			std::cin >> tx_data;
+			int sent_bytes = sendto(ServSock, tx_data.c_str(), tx_data.size(), 0,
+				(sockaddr*)&address, sizeof(sockaddr_in));
+			std::cout << "bytes send: " << sent_bytes << std::endl;
+		}
+		else if (cmd == "rx")
+		{
+			sockaddr_in tx_socket;
+			int tx_socket_length = sizeof(tx_socket);
+			char rx_buf[256];
+			int max_size_rx = sizeof(rx_buf);
+			int rx_bytes = recvfrom(ServSock, (char*)rx_buf, max_size_rx, 0,
+				(sockaddr*)&tx_socket, &tx_socket_length);
+			std::cout << "recive bytes: " << rx_bytes << std::endl;
+			if (rx_bytes > 0)
+			{
+				std::cout << "recive from ip: " << int_to_string_ip(htonl(tx_socket.sin_addr.s_addr)) << std::endl;
+				std::cout << "port: " << htons(tx_socket.sin_port) << std::endl;
+				printf("data:\n");
+				for (int i = 0; i < rx_bytes; i++)
+				{
+					std::cout << rx_buf[i];
+				}
+				printf("\n");
+			}
+		}
+		else if (cmd == "ex")
+		{
+			go = false;
+		}
+	}
+	closesocket(ServSock);
 }
